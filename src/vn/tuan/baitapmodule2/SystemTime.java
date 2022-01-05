b40e4baecdc51231fcf4fe165bc60b69a5ff0914
Nguyen Tuan <tuaniukiay98@gmail.com>
2022-01-05 21:13:56 +0700

    Date

---
+++ src/vn/tuan/baitapmodule2/SystemTime.java
@@ -0,0 +1,9 @@
+package vn.tuan.baitapmodule2;
+
+import java.util.Date;
+public class SystemTime {
+    public static void main(String[] args) {
+        Date now = new Date();
+        System.out.println("Date now: " + now);
+    }
+}
