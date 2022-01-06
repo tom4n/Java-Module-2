704748d8d6ca2483b879d4b58ade34c5280e2a73
Nguyen Tuan <tuaniukiay98@gmail.com>
2022-01-06 17:15:55 +0700

    chuyển đổi tiền tệ

---
+++ src/vn/tuan/baitapmodule2/USDtoVND.java
@@ -0,0 +1,15 @@
+package vn.tuan.baitapmodule2;
+
+import java.util.Scanner;
+
+public class USDtoVND {
+    public static void main(String[] args) {
+        double VND = 23000;
+        double USD;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhập số tiền USD: ");
+        USD = scanner.nextDouble();
+        double quydoi = USD * VND;
+        System.out.printf("VND có giá trị là: %f", quydoi);
+    }
+}
