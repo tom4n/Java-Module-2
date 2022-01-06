a36b5bfb9074afc461244de423091977d327b7b8
Nguyen Tuan <tuaniukiay98@gmail.com>
2022-01-06 08:40:20 +0700

    tính chỉ số cân nặng bmi

---
+++ src/vn/tuan/baitapmodule2/bmiCaculator.java
@@ -0,0 +1,29 @@
+package vn.tuan.baitapmodule2;
+
+import java.util.Scanner;
+
+public class bmiCaculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double weight, height, bmi;
+        System.out.print("type your weight (kilogram): ");
+        weight = scanner.nextDouble();
+        System.out.print("type your height (meter): ");
+        height = scanner.nextDouble();
+
+        bmi = weight / Math.pow(height, 2);
+        System.out.printf("%-20s %s", "bmi", "Interpretation\n");
+
+        if (bmi < 18.5) {
+            System.out.printf("%-20f %s", bmi, "Underweight");
+        } else if (bmi >= 18.5 && bmi < 25.0) {
+            System.out.printf("%-20f %s", bmi, "Normal");
+        } else if (bmi >= 25.0 && bmi < 30.0) {
+            System.out.printf("%-20f %s", bmi, "Overweight");
+        } else if (bmi > 30) {
+            System.out.printf("%-20f %s", bmi, "Obese");
+        }
+
+
+    }
+}
