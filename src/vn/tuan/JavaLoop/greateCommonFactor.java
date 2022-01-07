78f0c578f8bcee5fae5809c6d6177d02d5a8e9c6
Nguyen Tuan <tuaniukiay98@gmail.com>
2022-01-07 09:25:33 +0700

    tìm bội số chung lớn nhất

--- src/vn/tuan/JavaLoop/greateCommonFactor.java
+++ src/vn/tuan/JavaLoop/greateCommonFactor.java
@@ -1,2 +1,31 @@
-package vn.tuan.JavaLoop;public class greateCommonFactor {
+package vn.tuan.JavaLoop;
+
+import java.util.Scanner;
+
+public class greateCommonFactor {
+    public static void main(String[] args) {
+        int a;
+        int b;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a: ");
+        a = scanner.nextInt();
+        System.out.print("Enter b: ");
+        b = scanner.nextInt();
+
+        a = Math.abs(a);
+        b = Math.abs(b);
+
+        if (a == 0 || b == 0) {
+            System.out.println("No greatest common factor.");
+        }
+
+        while (a != b) {
+            if (a > b) {
+                a = a - b;
+            } else {
+                b = b - a;
+            }
+        }
+        System.out.println("Greatest common factor: " + a);
+    }
 }
